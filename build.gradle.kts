bd8deae676c0e0d04373d0d88fcc962386d2db69
Manfred Huber <manfred.huber@downdrown.at>
2022-12-30 12:42:24 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.7.7"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	id("com.vaadin") version "23.3.1"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+	kotlin("plugin.jpa") version "1.6.21"
+}
+
+group = "at.downdrown.diary"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+extra["vaadinVersion"] = "23.3.1"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("com.vaadin:vaadin-spring-boot-starter")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("io.micrometer:micrometer-registry-influx")
+	runtimeOnly("org.hsqldb:hsqldb")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("com.vaadin:vaadin-bom:${property("vaadinVersion")}")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}

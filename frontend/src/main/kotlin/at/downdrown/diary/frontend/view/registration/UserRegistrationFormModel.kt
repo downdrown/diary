ffb14f94179efdb9a6f916630587f118e3ec2ad6
Manfred Huber <manfred.huber@downdrown.at>
2023-01-05 13:33:19 +0100

    Add UserRegistrationFormModel

---
+++ frontend/src/main/kotlin/at/downdrown/diary/frontend/view/registration/UserRegistrationFormModel.kt
@@ -0,0 +1,41 @@
+package at.downdrown.diary.frontend.view.registration
+
+import at.downdrown.diary.api.user.User
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+data class UserRegistrationFormModel (
+    var id: Long?,
+    var username: String,
+    var firstname: String,
+    var lastname: String,
+    var birthdate: LocalDate?,
+    var email: String,
+    var password: String,
+    var confirmPassword: String
+) {
+    constructor() : this(
+        null,
+        "",
+        "",
+        "",
+        null,
+        "",
+        "",
+        ""
+    )
+
+    fun toUser(): User {
+        return User(
+            null,
+            username,
+            firstname,
+            lastname,
+            birthdate,
+            email,
+            LocalDateTime.now(),
+            null
+        )
+
+    }
+}

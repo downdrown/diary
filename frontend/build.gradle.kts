c6e171358abbdd3985aaebeb3b2d3c1f795a6c2c
Manfred Huber <manfred.huber@downdrown.at>
2023-03-05 13:37:38 +0100

    Add org.vaadin.addons.minicalendar:addon:1.3.0 dependency

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -4,10 +4,20 @@ plugins {
     id("com.vaadin") version "23.3.1"
 }
 
+repositories {
+    maven {
+        url = uri("https://maven.vaadin.com/vaadin-addons")
+    }
+}
+
 dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("com.vaadin:vaadin-spring-boot-starter")
+    implementation("org.vaadin.addons.minicalendar:addon:1.3.0") {
+        // TODO: Remove exclusion once the dependency is removed from the lib
+        exclude(group = "org.slf4j", module = "slf4j-simple")
+    }
 
     implementation(project(mapOf("path" to ":api")))
     runtimeOnly(project(mapOf("path" to ":backend")))

b80871be81bda01e24b8c73983c1f86d42732d34
Manfred Huber <manfred.huber@downdrown.at>
2023-04-09 15:39:59 +0200

    Add service function to evaluate authentication state

--- frontend/src/main/kotlin/at/downdrown/diary/frontend/service/AuthenticationService.kt
+++ frontend/src/main/kotlin/at/downdrown/diary/frontend/service/AuthenticationService.kt
@@ -3,6 +3,7 @@ package at.downdrown.diary.frontend.service
 import at.downdrown.diary.frontend.view.View
 import com.vaadin.flow.component.UI
 import com.vaadin.flow.server.VaadinServletRequest
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler
 import org.springframework.stereotype.Service
 
@@ -16,4 +17,7 @@ class AuthenticationService {
         SecurityContextLogoutHandler().logout(httpServletRequest, null, null)
     }
 
+    fun isAuthenticated(): Boolean {
+        return SecurityContextHolder.getContext().authentication.isAuthenticated
+    }
 }
